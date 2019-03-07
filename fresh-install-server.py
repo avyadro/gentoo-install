c01240f9b12e1f90d034f441c97aa01d7ed60e1e
Alexander Villalobos Yadro <alexandervy@gmail.com>
2019-03-07 13:37:44 -0600

    Update

--- fresh-install-server.py
+++ fresh-install-server.py
@@ -1,3 +1,11 @@
+from __future__ import print_function
+import subprocess
+import argparse
+import requests
+import multiprocessing
+from collections import OrderedDict
+import pprint
+
 class TMap(object):
 	def __init__(self,key,value):
 		 self.key=key
@@ -37,6 +45,12 @@ class GentooInstall(object):
 
 	def __init__(self):		 
 		self.version = "1.0.0"
+		self.dummy = True
+		self.rootdir = "/mnt/gentoo"
+		self.arch = "amd64"
+		self.cpus = multiprocessing.cpu_count()
+		self.repo="http://distfiles.gentoo.org/releases/"+self.arch+"/autobuilds/"
+		self.url_stage3 = self.repo+"latest-stage3-"+self.arch+".txt"
 		#################
 		# Disk  		#
 		#################
@@ -63,27 +77,27 @@ class GentooInstall(object):
 		self.lvms.put('swap', TLV('swapfs','-L 8G','swap') )
 		self.lvms.put('home', TLV('homefs','-L 8G','fs.ext4') )
 		self.lvms.put('root', TLV('rootfs','-l 100%VG','fs.ext4') )
-		# Stage
-		self.stage="http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-20190305T214502Z.tar.xz"
+		
+		
 	def shell(self,cmd):
 		print(cmd)
+		if not self.dummy:
+			subprocess.run(cmd, shell=True, executable='/bin/bash')
 
-		
-
-def revert():
-	print("=> Removing logical volumes according to the new schema")
-	for e in gentoo.lvms:
-		print("- "+e.name)    
-		cmd = "lvremove -y /dev/"+gentoo.lvm_vg+"/"+e.name
-		gentoo.shell(cmd)
-	print("=> Removing volume group")
+def preparing_disk_revert(option, opt, value, parser):
+	print("=> Reverting preparing disk")
+	print("=> Unmounting the root partition")
+	e = gentoo.lvms.get('root')
+	cmd = "umount /dev/"+gentoo.lvm_vg+"/"+e.value.name
+	gentoo.shell(cmd)
+	print("=> Removing the logical and group volumes")
 	cmd = "vgremove -y "+gentoo.lvm_vg
 	gentoo.shell(cmd)
-	print("=> Removing physical volume")
+	print("=> Removing the physical volume")
 	cmd = "pvremove -y "+gentoo.disk+str(gentoo.p_lvm)
 	gentoo.shell(cmd)
 
-def preparing_disk():
+def preparing_disk(option, opt, value, parser):
 	print("#######################")
 	print("# Preparing the disks #")
 	print("#######################")
@@ -132,23 +146,91 @@ def preparing_disk():
 	cmd = "mount /dev/"+gentoo.lvm_vg+"/"+e.value.name+" /mnt/gentoo"
 	gentoo.shell(cmd)
 # Installing a stage tarball
-def installing_satage():
+def installing_stage(option, opt, value, parser):
 	print("##############################")
 	print("# Installing a stage tarball #")
 	print("##############################")
 	print("=> Setting the date and time")
 	cmd="ntpd -q -g"
-	gentoo.shell(cmd)
-	print("=> Entering /mnt/gentoo directory")
-	cmd = "cd /mnt/gentoo"
-	gentoo.shell(cmd)
+	gentoo.shell(cmd)	
 	print("=> Downloading stage")
-	cmd = "wget -c "+gentoo.stage
-	gentoo.shell(cmd)
+	response = requests.get(gentoo.url_stage3).text.splitlines()	
+	cmd = "wget -P "+gentoo.rootdir+" -c "+gentoo.repo+response[-1].split(" ")[0]
+	gentoo.shell(cmd)	
 	print("=> Unpacking the stage tarball")
-	cmd = "tar xpvf stage3-*.tar.bz2 --xattrs-include='*.*' --numeric-owner"
+	cmd = "tar xpvf "+gentoo.rootdir+"/stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner -C "+gentoo.rootdir
+	gentoo.shell(cmd)
 
 gentoo = GentooInstall()
 
+from optparse import OptionParser
+
+
+usage = "usage: %prog [options] arg"
+parser = OptionParser(usage)
+#parser.add_option("-f", "--file", dest="filename",
+#					help="read data from FILENAME")
+#parser.add_option("-v", "--verbose",
+#					action="store_true", dest="verbose")
+#parser.add_option("-q", "--quiet",
+#					action="store_false", dest="verbose")
+parser.add_option("-d", "--preparing-disk",
+					help="make partitions according to schema.",
+					action="callback",
+					callback=preparing_disk)
+parser.add_option("-D", "--preparing-disk-revert",
+					help="revert partitions created.",
+					action="callback",
+					callback=preparing_disk_revert)
+parser.add_option("-i", "--install-stage",
+					help="installing stage3.",
+					action="callback",
+					callback=installing_stage)
+					
+(options, args) = parser.parse_args()
+
+
+
+#parser = argparse.ArgumentParser(description='Gentoo AMD64 Handbook :: Basic Install Process')
+
+#parser.add_argument('--preparing-disk',
+#					default='10', type=int, nargs=1,
+#                    help='To be able to install Gentoo, the necessary partitions need to be created.')
+#parser.add_argument('--preparing-disk',
+#					'-pdsk',
+#					help='To be able to install Gentoo, the necessary partitions need to be created.',
+#					action="count")
+#parser.add_argument('--verbose', '-v', action='count')
+#
+#args = parser.parse_args()
+
+
 # preparing_disk()
-installing_satage()
+# installing_satage()
+
+
+def cpuinfo():
+ cpuinfo=OrderedDict()
+ procinfo=OrderedDict()
+ nprocs = 0
+ with open('/proc/cpuinfo') as f:
+  for line in f:
+   if not line.strip():
+    cpuinfo['proc%s' % nprocs] = procinfo
+    nprocs=nprocs+1
+    procinfo=OrderedDict()
+   else:
+    if len(line.split(':')) == 2:
+     procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
+    else:
+     procinfo[line.split(':')[0].strip()] = ''
+ return cpuinfo
+
+cpuinfo = cpuinfo()['proc0']
+print(cpuinfo)
+
+
+print("Vendor ID: "+cpuinfo['vendor_id']) 
+print("CPU family: "+cpuinfo['cpu family']) 
+print("Model: "+cpuinfo['model']) 
+print("Model name: "+cpuinfo['model name']) 
